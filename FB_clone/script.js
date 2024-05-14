9c3e6f4532d6e0e5483bf810b1bf856266d9468b
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-05-14 01:43:56 +0100

    fb_clone

---
+++ FB_clone/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const passwordbox = document.getElementById("password");
+  const checkbox = document.getElementById("checkbox");
+
+  checkbox.addEventListener("change", () => {
+    // Toggle the password visibility based on checkbox state
+    if (checkbox.checked) {
+      passwordbox.type = "text"; // Show password
+    } else {
+      passwordbox.type = "password"; // Hide password
+    }
+  });
+});
+console.log("BornToWin");
